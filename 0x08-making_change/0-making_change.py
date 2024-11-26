688c1574a5f03c1c1ed6bc6f53b9b59fb21fa449
Gemechu T <gammachistesfaye@gmail.com>
2024-11-26 11:15:11 +0300

    Making changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,47 @@
 #!/usr/bin/python3
-
-""" Contains makeChange function"""
+"""Module for Making Change.
+"""
 
 
 def makeChange(coins, total):
+    """Calculates the fewest number of coins needed to meet a given total.
+
+    If total is 0 or less, returns 0. If total cannot be met by any number
+    of coins, returns -1.
+    Assumes that coins is a list of the values of the coins in your
+    possession, the value of a coin is always an integer greater than 0,
+    and you have an infinite number of each denomination of coin.
+
+    Args:
+        coins (list[int]): A list of coin values.
+        total (int): The total amount of money to make.
+
+    Returns:
+        int: The fewest number of coins needed to meet the total.
     """
-    Returns: fewest number of coins needed to meet total
-        If total is 0 or less, return 0
-        If total cannot be met by any number of coins you have, return -1
-    """
-    if not coins or coins is None:
-        return -1
+    # If the total is zero or less, we don't need any coins
     if total <= 0:
         return 0
-    change = 0
-    coins = sorted(coins)[::-1]
+    # Sort the coins in decreasing order
+    coins.sort(reverse=True)
+    # Initialize the coin count to zero
+    coin_count = 0
+    # Iterate through each coin in the list of coins
     for coin in coins:
-        while coin <= total:
-            total -= coin
-            change += 1
-        if (total == 0):
-            return change
-    return -1
+        # If the coin is greater than the remaining total, skip it
+        if coin > total:
+            continue
+        # Calculate the number of times the current coin can be used
+        count = total // coin
+        # Update the total by subtracting the value of the coins used
+        total -= count * coin
+        # Update the coin count by adding the number of coins used
+        coin_count += count
+        # If the total is now zero, we're done
+        if total == 0:
+            break
+    # If we couldn't make change for the total, return -1
+    if total > 0:
+        return -1
+    # Otherwise, return the coin count
+    return coin_count
