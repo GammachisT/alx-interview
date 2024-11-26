2b4cf216f42686127070d1cce34ec8c63bc1fc32
Gemechu T <gammachistesfaye@gmail.com>
2024-11-26 11:08:06 +0300

    Making changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,40 +1,25 @@
 #!/usr/bin/python3
-"""
-Module to calculate the fewest number of coins needed to meet a total
-"""
+""" Making changes """
+
 
 def makeChange(coins, total):
-    """
-    Calculate the fewest number of coins needed to meet total.
+    """ Generate changes needed to reach total
 
     Args:
-        coins (list): A list of the values of the coins.
-        total (int): The total amount to be met with coins.
-
-    Returns:
-        int: The fewest number of coins needed to meet total,
-             -1 if total cannot be met by any number of coins,
-             0 if total is 0 or less.
+        coins ([List]): [List of Coins available]
+        total ([int]): [total amount needed]
     """
     if total <= 0:
         return 0
-
-    # Initialize a list to hold the minimum coins needed for each amount
-    min_coins = 0
-    temp = 0  # No coins needed to make 0
-
+    check = 0
+    temp = 0
     coins.sort(reverse=True)
-    # Iterate through each coin
-
-    for coin in coins:
+    for i in coins:
         while check < total:
-            min_coins += coin
+            check += i
             temp += 1
-            # Update the minimum coins needed for this amount
-            if min_coins == total:
-                return temp
-            min_coins -= coin
-            min_coins -= 1
-
-    # If we can't make the total, return -1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
     return -1
